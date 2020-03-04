61608146ef5a7a47dcfdfc650d5b203f5f36f91c
Safia Nuzhath <safia@Safias-MacBook-Home.local>
2020-03-04 12:14:59 -0800

    Check if list contains items

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const trnasformedIngredients = Object.keys(props.ingredients)
+    let trnasformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if (trnasformedIngredients.length === 0) {
+        trnasformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
+    console.log(trnasformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
